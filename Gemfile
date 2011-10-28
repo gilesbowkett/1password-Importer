a6c0b58d626e1b19fa2a501928150ec0a28367db
Giles Bowkett <gilesb@gmail.com>
2011-10-28 13:07:23 -0700

    convert yaml passwords to csv for 1password

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source :rubygems
+
+gem "awesome_print"
--- passwords.rb
+++ passwords.rb
@@ -1,3 +1,11 @@
-File.open("passwords.yml").each_line do |line|
-  puts line
+require 'yaml'
+require 'rubygems'
+require 'ap'
+
+passwords = YAML::load(File.open("passwords.yml"))
+
+puts "account name, username, password"
+passwords.each do |account_name, user_pass_hash|
+  account_name = "old_skool_" + account_name
+  puts account_name + "," + user_pass_hash[:username].to_s + "," + user_pass_hash[:password].to_s
 end
